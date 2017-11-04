4a6a81f0e00caf415ef4c148b535fb9fe087a399
GabrielCee27 <gabrielcee81@gmail.com>
2017-11-03 19:21:56 -0700

    3 sliders added.

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -28,35 +28,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSwitch: UISwitch!
     @IBOutlet weak var blueSwitch: UISwitch!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     func updateColor(){
         var red : CGFloat = 0;
         var green : CGFloat = 0;
         var blue : CGFloat = 0;
         
         if redSwitch.isOn {
-            red = 1;
+            red = CGFloat(redSlider.value)
         }
         if greenSwitch.isOn {
-            green = 1;
+            green = CGFloat(greenSlider.value)
         }
         if blueSwitch.isOn {
-            blue = 1;
+            blue = CGFloat(blueSlider.value)
         }
         
         let color = UIColor(red: red, green: green, blue: blue, alpha: 1);
-        
         colorView.backgroundColor = color;
-        
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        updateColor();
     }
     
     @IBAction func switchChanged(_ sender: UISwitch) {
         updateColor();
     }
-    
-
-    
-    
-    
-    
+ 
 }
 
