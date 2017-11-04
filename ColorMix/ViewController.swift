4635d180d03c124784b0499b373e42303b473c8b
GabrielCee27 <gabrielcee81@gmail.com>
2017-11-03 18:50:31 -0700

    Two more switches added.

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        colorView.backgroundColor = .black
-        
+        updateColor();
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,13 +24,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
-    @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    func updateColor(){
+        var red : CGFloat = 0;
+        var green : CGFloat = 0;
+        var blue : CGFloat = 0;
+        
+        if redSwitch.isOn {
+            red = 1;
         }
+        if greenSwitch.isOn {
+            green = 1;
+        }
+        if blueSwitch.isOn {
+            blue = 1;
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1);
+        
+        colorView.backgroundColor = color;
+        
     }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor();
+    }
+    
+
+    
+    
     
     
 }
